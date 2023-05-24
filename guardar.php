2ba0a3e3276a7447fb92869751b97109184f9a67
willfs144 <willfs144@gmail.com>
2023-05-23 22:19:03 -0500

    Se agrega telefono y se guarda en el archivo txt

--- guardar.php
+++ guardar.php
@@ -2,13 +2,15 @@
 if ($_SERVER['REQUEST_METHOD'] === 'POST') {
     $nombre = $_POST['nombre'];
     $email = $_POST['email'];
+    $telefono = $_POST['telefono'];
     $mensaje = $_POST['mensaje'];
 
     // Ruta del archivo de texto donde se guardarán los datos
     $archivo = 'contactos.txt';
 
     // Crear el contenido del registro
-    $registro = "Nombre: $nombre\nEmail: $email\nMensaje: $mensaje\n\n";
+      $registro = "Nombre: $nombre\nEmail: $email\nTelefono: $telefono\nMensaje: $mensaje\n\n";
+
 
     // Guardar el registro en el archivo de texto
     file_put_contents($archivo, $registro, FILE_APPEND | LOCK_EX);
